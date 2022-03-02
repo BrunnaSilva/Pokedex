5d9203085e7f8df9134a2910d7a593fed50546ca
brunnasilva <bruna.silva@dcomp.ufs.br>
2022-03-01 21:52:26 -0300

    chore: settings theme

---
+++ src/styles/images.ts
@@ -0,0 +1,6 @@
+const images = {
+  logo: require('../assets/icons/Vector.png'),
+  text_pokedex: require('../assets/icons/ioasys_pokedex.png')
+}
+
+export default images
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    background: '#F7F7F7',
+    background_dark: '#212121',
+    secondary: '#EC0344',
+    button_gray: '#8D8B92',
+    medium_gray: '#666666'
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
+  },
+};
