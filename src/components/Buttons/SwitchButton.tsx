291665bd308648b4aa01a33032ea432f42cbcd38
brunnasilva <bruna.silva@dcomp.ufs.br>
2022-03-01 22:00:28 -0300

    chore: adjust components

--- src/components/Buttons/SwitchButton.tsx
+++ src/components/Buttons/SwitchButton.tsx
@@ -3,18 +3,15 @@ import { View, Switch } from 'react-native'
 import theme from '../../styles/theme'
 
 interface SwitchProps {
-  isVisible: boolean,
+  isVisible: boolean
   onPress: () => void
 }
 
 const SwitchButton = ({ isVisible, onPress }: SwitchProps) => {
-  //const [isEnabled, setIsEnabled] = useState(false);
-  //const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
   return (
     <View>
       <Switch
-        trackColor={{ false: theme.colors.secondary , true: theme.colors.secondary }}
+        trackColor={{ false: theme.colors.secondary, true: theme.colors.secondary }}
         thumbColor={isVisible ? theme.colors.secondary : theme.colors.button_gray}
         ios_backgroundColor='white'
         onValueChange={onPress}
